324721b0882a862130e1928c3ec4422bee59ba8e
RobertHan96 <yshan4329@gmail.com>
2020-04-13 21:47:18 +0900

    2nd commit : List XML, 어뎁터 생성, 리스트뷰 연결

--- app/src/main/java/com/nexon/playstoreclone/MainActivity.kt
+++ app/src/main/java/com/nexon/playstoreclone/MainActivity.kt
@@ -2,11 +2,24 @@ package com.nexon.playstoreclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nexon.playstoreclone.adapters.AppAdapter
+import com.nexon.playstoreclone.datas.App
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val appList : ArrayList<App>()
+    var mAppAdapter : AppAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appList.add(App("아스팔트7","GameLoft", 4))
+        appList.add(App("팔라독","NCSoft", 1))
+        appList.add(App("리니지M","Netmarble", 4))
+        appList.add(App("V4","NexonKorea", 5))
+
+        mAppAdapter = AppAdapter(this, R.layout.app_list_item, appList)
+        appListView.adapter = mAppAdapter
     }
 }
--- app/src/main/java/com/nexon/playstoreclone/adapters/AppAdapter.kt
+++ app/src/main/java/com/nexon/playstoreclone/adapters/AppAdapter.kt
@@ -1,4 +1,36 @@
 package com.nexon.playstoreclone.adapters
 
-class AppAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.nexon.playstoreclone.R
+import com.nexon.playstoreclone.datas.App
+
+class AppAdapter (context: Context, resID : Int, list: ArrayList<App>) : ArrayAdapter<App>(context, resID, list) {
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        tempRow?.let{ 
+            // null이 아닐때
+        }.let {
+            // null일때
+            tempRow = inf.inflate(R.layout.app_list_item, null)           
+        }
+
+//    위에서 이미 null 체크가 끝났으므로 내가 가져올 리스트 아이템은 !!를 사용해 강제 언랩핑해서 사용 준비
+        val row = tempRow!!
+
+
+
+        return  row
+    }
+
+
 }
+
