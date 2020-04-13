79069c17a1b5d13b8223bc205171f1ce23830e02
RobertHan96 <yshan4329@gmail.com>
2020-04-13 22:02:35 +0900

    3th commit : 어뎁터와 리스트뷰 연결

--- app/src/main/java/com/nexon/playstoreclone/MainActivity.kt
+++ app/src/main/java/com/nexon/playstoreclone/MainActivity.kt
@@ -7,17 +7,22 @@ import com.nexon.playstoreclone.datas.App
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val appList : ArrayList<App>()
+    val appList = ArrayList<App>()
     var mAppAdapter : AppAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        appList.add(App("아스팔트7","GameLoft", 4))
-        appList.add(App("팔라독","NCSoft", 1))
-        appList.add(App("리니지M","Netmarble", 4))
-        appList.add(App("V4","NexonKorea", 5))
+        appList.add(App("아스팔트8 : 에어본","GameLoft", 5))
+        appList.add(App("MineCraft ","Mojang ", 4))
+        appList.add(App("아스팔트 7 : 히트","GameLoft", 4))
+        appList.add(App("팔라독 (Paladog)","FazeCat ", 2))
+        appList.add(App("Plants vs. Zombies","EA Games", 3))
+        appList.add(App("스왐피 (Swampy)","Disney", 5))
+        appList.add(App("리니지2M","NCSOFT", 3))
+        appList.add(App("리니지M","NCSOFT", 4))
+        appList.add(App("AFK아레나","Lilith Games", 1))
 
         mAppAdapter = AppAdapter(this, R.layout.app_list_item, appList)
         appListView.adapter = mAppAdapter
--- app/src/main/java/com/nexon/playstoreclone/adapters/AppAdapter.kt
+++ app/src/main/java/com/nexon/playstoreclone/adapters/AppAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.nexon.playstoreclone.R
 import com.nexon.playstoreclone.datas.App
+import kotlinx.android.synthetic.main.app_list_item.view.*
 
 class AppAdapter (context: Context, resID : Int, list: ArrayList<App>) : ArrayAdapter<App>(context, resID, list) {
     val mContext = context
@@ -26,7 +27,19 @@ class AppAdapter (context: Context, resID : Int, list: ArrayList<App>) : ArrayAd
 //    위에서 이미 null 체크가 끝났으므로 내가 가져올 리스트 아이템은 !!를 사용해 강제 언랩핑해서 사용 준비
         val row = tempRow!!
 
-
+//        val nameText = row.findViewById<TextView>(R.id.titleText)
+//        val companyNameText = row.findViewById<TextView>(R.id.companyText)
+//        val starOneImg = row.findViewById<TextView>(R.id.starOneImg)
+//        val starTwoImg = row.findViewById<TextView>(R.id.starTwoImg)
+//        val starThreeImg = row.findViewById<TextView>(R.id.starThreeImg)
+//        val starFourImg = row.findViewById<TextView>(R.id.starFourImg)
+//        val starFiveImg = row.findViewById<TextView>(R.id.starFiveImg)
+//
+//        val data = mList.get(position)
+//
+//
+//        nameText.text = data.title
+//        companyNameText.text = data.company
 
         return  row
     }
