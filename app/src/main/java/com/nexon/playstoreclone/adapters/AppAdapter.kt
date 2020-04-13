5fb1d2bf09633ccf841fbb37b4790857ddc4d218
RobertHan96 <yshan4329@gmail.com>
2020-04-13 22:20:17 +0900

    4th commit : 최종 리스트뷰 예시 구현

--- app/src/main/java/com/nexon/playstoreclone/adapters/AppAdapter.kt
+++ app/src/main/java/com/nexon/playstoreclone/adapters/AppAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import com.nexon.playstoreclone.R
 import com.nexon.playstoreclone.datas.App
@@ -26,20 +27,36 @@ class AppAdapter (context: Context, resID : Int, list: ArrayList<App>) : ArrayAd
 
 //    위에서 이미 null 체크가 끝났으므로 내가 가져올 리스트 아이템은 !!를 사용해 강제 언랩핑해서 사용 준비
         val row = tempRow!!
+//     findViewById를 할때는 찾을 객체의 속성을 <> 안에 정확히 입력해야 객체를 가져올 수 있음
 
-//        val nameText = row.findViewById<TextView>(R.id.titleText)
-//        val companyNameText = row.findViewById<TextView>(R.id.companyText)
-//        val starOneImg = row.findViewById<TextView>(R.id.starOneImg)
-//        val starTwoImg = row.findViewById<TextView>(R.id.starTwoImg)
-//        val starThreeImg = row.findViewById<TextView>(R.id.starThreeImg)
-//        val starFourImg = row.findViewById<TextView>(R.id.starFourImg)
-//        val starFiveImg = row.findViewById<TextView>(R.id.starFiveImg)
-//
-//        val data = mList.get(position)
-//
-//
-//        nameText.text = data.title
-//        companyNameText.text = data.company
+        val nameText = row.findViewById<TextView>(R.id.titleText)
+        val companyNameText = row.findViewById<TextView>(R.id.companyText)
+        val starOneImg = row.findViewById<ImageView>(R.id.starOneImg)
+        val starTwoImg = row.findViewById<ImageView>(R.id.starTwoImg)
+        val starThreeImg = row.findViewById<ImageView>(R.id.starThreeImg)
+        val starFourImg = row.findViewById<ImageView>(R.id.starFourImg)
+        val starFiveImg = row.findViewById<ImageView>(R.id.starFiveImg)
+
+        val data = mList.get(position)
+
+        val starImgList = ArrayList<ImageView>()
+        starImgList.add(starOneImg)
+        starImgList.add(starTwoImg)
+        starImgList.add(starThreeImg)
+        starImgList.add(starFourImg)
+        starImgList.add(starFiveImg)
+
+        for (index in 0..4) {
+            val starImg = starImgList.get(index)
+            if (index < data.score) {
+                starImg.setImageResource(R.drawable.fill_star)
+            } else {
+                starImg.setImageResource(R.drawable.empty_star)
+            }
+        }
+
+        nameText.text = "${position+1}. ${data.title}"
+        companyNameText.text = data.company
 
         return  row
     }
