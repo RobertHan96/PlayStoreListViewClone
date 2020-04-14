c4c0fe2485e45d76e5a097540aff5b4d2c1ffb63
RobertHan96 <yshan4329@gmail.com>
2020-04-14 19:59:34 +0900

    5th commit : 코드 구조 정리

--- app/src/main/java/com/nexon/playstoreclone/MainActivity.kt
+++ app/src/main/java/com/nexon/playstoreclone/MainActivity.kt
@@ -13,7 +13,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
 
+
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+        addApps()
+        mAppAdapter = AppAdapter(this, R.layout.app_list_item, appList)
+        appListView.adapter = mAppAdapter
+    }
+
+    fun addApps() {
         appList.add(App("아스팔트8 : 에어본","GameLoft", 5))
         appList.add(App("MineCraft ","Mojang ", 4))
         appList.add(App("아스팔트 7 : 히트","GameLoft", 4))
@@ -23,8 +39,6 @@ class MainActivity : AppCompatActivity() {
         appList.add(App("리니지2M","NCSOFT", 3))
         appList.add(App("리니지M","NCSOFT", 4))
         appList.add(App("AFK아레나","Lilith Games", 1))
-
-        mAppAdapter = AppAdapter(this, R.layout.app_list_item, appList)
-        appListView.adapter = mAppAdapter
+        appList.add(App("아스팔트8 : 에어본","GameLoft", 5))
     }
 }
