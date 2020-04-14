30c0b7425266b85cdb0195a21524608d06cb2d9b
RobertHan96 <yshan4329@gmail.com>
2020-04-14 20:21:02 +0900

    6th commit : Alert창 구현

--- app/src/main/java/com/nexon/playstoreclone/MainActivity.kt
+++ app/src/main/java/com/nexon/playstoreclone/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.nexon.playstoreclone
 
+import android.content.DialogInterface
+import android.content.DialogInterface.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import com.nexon.playstoreclone.adapters.AppAdapter
 import com.nexon.playstoreclone.datas.App
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,7 +24,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+        appListView.setOnItemLongClickListener { parent, view, position, id ->
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("삭제 확인")
+            alert.setMessage("정말 이앱을 삭제하시겠습니까?")
+            alert.setPositiveButton("확인", {dialog, which ->
+                Log.d("앱목록 삭제 전", appList.size.toString())
+                appList.removeAt(position)
+                Log.d("앱목록 삭제 후", appList.size.toString())
+//          원본 데이터에 변경이 생긴 경우 어댑터에 데이터 갱신을 요청하는 코드
+                mAppAdapter?.notifyDataSetChanged()
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+            return@setOnItemLongClickListener true
 
+        }
     }
 
     fun setValues() {
